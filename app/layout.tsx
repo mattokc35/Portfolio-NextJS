0fb2af251e78696c1f49268fd902e9749e62a1ef
Matthew Chen <mattokc35@gmail.com>
2024-01-31 00:39:27 -0600

    gradient background

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-slate-100 text-gray-950 relative h-[5000px]`}
       >
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <div className="absolute inset-0 bg-gradient-to-r from-[#fbe2e3] via-[#dbd7fb] to-slate-100 animate-gradient -z-10"></div>
         <Header />
         {children}
       </body>
