5b070f26a0f31538be6cf9c1bb0cc1acad586d35
Matthew Chen <mattokc35@gmail.com>
2024-02-04 12:56:47 -0600

    next redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/index.html",
+        destination: "/",
+        permanent: false, // Set to true if you want a permanent redirect (301)
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
