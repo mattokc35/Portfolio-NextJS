a3fa9c506c3da4c0a1266e795bbb50b2df8e1cf2
Matthew Chen <mattokc35@gmail.com>
2024-09-04 01:04:30 -0500

    update resume

--- components/intro.tsx
+++ components/intro.tsx
@@ -65,7 +65,7 @@ export default function Intro() {
       >
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-100 transition cursor-pointer border border-black/10"
-          href="/Resume4.pdf"
+          href="/Matthew-Chen-Resume-2.pdf"
           download={true}
         >
           Download Resume{" "}
